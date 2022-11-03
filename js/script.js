5fca7375da9c0ac01fa8ca630b6b89dc0078bf75
Dev Chauhan <devccode8@gmail.com>
2022-11-03 22:13:44 +0530

    project upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var number1=Math.floor(((Math.random())*6)+1);
+var number2=Math.floor(((Math.random())*6)+1);
+var image1="img/Dice"+number1+".png";
+var image2="img/Dice"+number2+".png";
+var img1=document.querySelectorAll("img")[0];
+img1.setAttribute("src",image1);
+var img2=document.querySelectorAll("img")[1];
+img2.setAttribute("src",image2);
+if (number1>number2) {
+    document.querySelector("h2").innerHTML="🥳 Player 1 win"
+} else if(number2>number1){
+    document.querySelector("h2").innerHTML="Player 2 win 🥳 "
+}else{
+    document.querySelector("h2").innerHTML="Match Draw"
+}
